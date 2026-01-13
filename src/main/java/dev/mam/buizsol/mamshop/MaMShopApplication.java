8c071f8130338597abccb1b94f277162ef19465b
Bohdan Nerushev <bnerushev@united-internet.de>
2026-01-13 10:42:03 +0100

    feat(mamshop): initialize Maven project and create MaMShopApplication class
    Created the first commit, initialized the Maven project structure, and added the main MaMShopApplication class.
    
    Refs: BUIZSOL-69750

---
+++ src/main/java/dev/mam/buizsol/mamshop/MaMShopApplication.java
@@ -0,0 +1,8 @@
+package dev.mam.buizsol.mamshop;
+
+public class MaMShopApplication {
+
+    public static void main(String[] args) {
+        System.out.println("MaM Shop Application started...");
+    }
+}
