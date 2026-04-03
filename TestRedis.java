732f5f5566bff557587df60bda4859fb8fa07f61
Bohdan Nerushev <bnerushev@united-internet.de>
2026-04-03 15:00:06 +0200

    chore(git): add agent skills files to ignore and remove TestRedis
    Update .gitignore to exclude Agent Skills configurations. Remove temporary TestRedis.java class as it is no longer needed.

--- TestRedis.java
+++
@@ -1,9 +0,0 @@
-import org.springframework.data.redis.serializer.RedisSerializer;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-public class TestRedis {
-    public void test() {
-        ObjectMapper m = new ObjectMapper();
-        RedisSerializer<Object> s = RedisSerializer.json(m);
-    }
-}
