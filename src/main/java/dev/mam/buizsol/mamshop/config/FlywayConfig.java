834b7ef2dce965edbb5b15e3892a6ff701a0acd0
Bohdan Nerushev <bnerushev@united-internet.de>
2026-07-18 14:08:18 +0200

    fix(tests): resolve integration test context failures by mocking JwtDecoder and enabling Flyway

--- src/main/java/dev/mam/buizsol/mamshop/config/FlywayConfig.java
+++ src/main/java/dev/mam/buizsol/mamshop/config/FlywayConfig.java
@@ -1,14 +1,15 @@
 package dev.mam.buizsol.mamshop.config;
 
+import javax.sql.DataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.flywaydb.core.Flyway;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-
 @Slf4j
 @Configuration
+@ConditionalOnProperty(name = "spring.flyway.enabled", havingValue = "true", matchIfMissing = true)
 public class FlywayConfig {
 
     @Bean
